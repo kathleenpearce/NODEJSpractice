428955e8881709ad300aa3b6520b12ade86b9358
Kathleen pearce <kathleenpearce@live.com>
2019-06-20 20:10:08 +0000

    added projects page

--- router/main.js
+++ router/main.js
@@ -6,4 +6,7 @@ module.exports = function(app)
      app.get('/about',function(req,res){
         res.render('about.html');
     });
+    app.get('/projects',function(req, res){
+      res.render('projects.html');
+    });
 }
